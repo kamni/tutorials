64cf86e6a9c58d4ba962e4a89b1e8dbb84affe6b
J Leadbetter <j@jleadbetter.com>
2023-01-14 11:34:34 +0100

    [2023-01 pyscript] example 05

--- 2023-01_pyscript/static/script/nav.py
+++ 2023-01_pyscript/static/script/nav.py
@@ -3,6 +3,8 @@ NAV_LINKS = (
     ('/01-imports.html', '01 Imports'),
     ('/02-paths.html', '02 Paths'),
     ('/03-paths2.html', '03 More Paths'),
+    ('/04-repl.html', '04 REPL'),
+    ('/05-repl2.html', '05 More REPL'),
 )
 
 navlist = '<div class="nav"><ul>'
